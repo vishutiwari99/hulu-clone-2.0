4d145475e87c0e115d2c38888e19c5a800750bb0
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-05-13 16:48:39 +0530

    Change in Image src.

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,19 +5,14 @@ import { forwardRef } from "react";
 
 
 const Thumbnail = forwardRef(({ result }, ref) => {
-    console.log("Yeh Thumbnail hai", result);
     const BASE_URL = "https://image.tmdb.org/t/p/original"
-    // var { backdrop_path } = result;
-    // console.log("yeh image location", backdrop_path);
     var url = `${BASE_URL}${result.backdrop_path}`;
-    // console.log("yeh url hai re baba ", url);
-    console.log('yeh url hai', url);
 
     return (
         <div
             ref={ref}
             className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-            <Image alt={result.backdrop_path} layout='responsive' src={`https://image.tmdb.org/t/p/original${result.backdrop_path}`} height={1000} width={1920} />
+            <Image alt={result.backdrop_path} layout='responsive' src={url} height={1000} width={1920} />
             <div className="p-2">
                 <p className="truncate max-w-md">{result.overview}</p>
                 <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
